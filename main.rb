fbc8338d7b2f23ea1ff69c6f12bb52b54856c17f
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-12-18 18:12:35 +0900

    リファクタリング

--- main.rb
+++ main.rb
@@ -7,13 +7,11 @@ brave = Brave.new(name: "ゆうしゃ", hp: 300, offense: 300, defense: 100)
 monster = Monster.new(name: "アークデーモン", hp: 300, offense: 300, defense: 100)
 
 puts "#{monster.name} があらわれた！"
-
 while brave.hp > 0 && monster.hp > 0 do
   brave.attack(monster)
   monster.attack(brave) if monster.hp > 0
   character.info(brave, monster)
-  puts "#{brave.name} はしんでしまった！" if brave.hp == 0
-  puts "#{monster.name} をやっつけた！" if monster.hp == 0
 end
-
+puts "#{brave.name} はしんでしまった！" if brave.hp == 0
+puts "#{monster.name} をやっつけた！" if monster.hp == 0
 
